db86f8669bb02f5dcc9d4dd8783b6caac5eaebfa
Awembom <nkimihawembom@gmail.com>
2024-09-27 16:16:39 +0100

    adding the pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navtop from "../components/navtop/Navtop";
+import Ourpillars from "../components/ourpillars/Ourpillars";
+import Whoweare from "../components/whoweare/Whoweare";
+import Oursupport from "../components/oursupport/Oursupport";
+import Partners from "../components/partners/Partners";
+import Inpartners from "../components/inpartners/Inpartners";
+import Newsletter from "../components/newsletter/Newsletter";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navtop />
+      <Ourpillars />
+      <Whoweare />
+      <Oursupport />
+      <Partners />
+      <Inpartners />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
