aef73e1bb1fa795c9ee612afbfc1affdcbdc63aa
Awembom <nkimihawembom@gmail.com>
2024-10-11 22:38:38 +0100

    adding images on slider

--- src/pages/aboutevent/Aboutevent.jsx
+++ src/pages/aboutevent/Aboutevent.jsx
@@ -3,9 +3,23 @@ import "./aboutevent.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import biliImage from "../../assets/Lakdbili.png";
+
+// Start of slider images import
+import stud from "../../assets/slide2.jpg";
+import women from "../../assets/slide3.jpg";
+import sittingPeople from "../../assets/image two.jpg";
+import tradPeople from "../../assets/slide5.jpg";
 import image2 from "../../assets/juju.png";
 import image3 from "../../assets/schooluniversity.png";
+import school from "../../assets/slide4.jpg";
+import isoschool from "../../assets/isoschool.jpg";
+import sattimg1 from "../../assets/sattimg1.jpg";
+import sattimg2 from "../../assets/sattimg2.jpg";
+import gogmap from "../../assets/mapimg.jpg";
+import biliImage from "../../assets/The_Lake_Bambili_1.jpg";
+
+// End of Slider Images import
+
 // import Varpillarcard from "../../components/pillarcards/varpillarcard/Varpillarcard";
 import Abeventpillars from "../../components/abouteventpillars/Abeventpillars";
 import ExternalR from "../../components/pillarexternal/ExternalR";
@@ -15,16 +29,16 @@ const Aboutevent = () => {
     infinite: true,
     speed: 500,
     autoplay: true, // Enable autoplay
-    autoplaySpeed: 5000,
+    autoplaySpeed: 7000,
     slidesToShow: 2,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
           infinite: true,
           // dots: true,
         },
@@ -54,12 +68,57 @@ const Aboutevent = () => {
       <div className="slider-container">
         <Slider {...settings}>
           <div className="aboutevent-container-image">
-            <img src={image3} alt="" />
+            <img src={stud} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={women} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={sittingPeople} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={tradPeople} alt="" />
             <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
           </div>
 
           <div className="aboutevent-container-image">
             <img src={image2} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={image3} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={school} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={isoschool} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={sattimg1} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={sattimg2} alt="" />
+            <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
+          </div>
+
+          <div className="aboutevent-container-image">
+            <img src={gogmap} alt="" />
             <div className="image-lebel">{/* <p>Lake Bambili</p> */}</div>
           </div>
 
